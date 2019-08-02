4539e01e92fd2319ff81eea9d148df6b7438206e
Zach Boyle <zboyle@umich.edu>
2019-08-02 18:42:46 -0400

    put app on heroku

--- app.js
+++ app.js
@@ -11,20 +11,20 @@ app.get('/', function(req, res) {
 });
 
 // FROM UMICH CODE DOCS
-var options = { method: 'GET',
-  url: 'https://apigw.it.umich.edu/um/Curriculum/SOC/Terms/REPLACE_TERMCODE/Schools/REPLACE_SCHOOLCODE/Subjects/REPLACE_SUBJECTCODE/CatalogNbrs/REPLACE_CATALOGNUMBER/Sections/REPLACE_SECTIONNUMBER/Meetings',
-  headers: 
-   { accept: 'application/json',
-     authorization: 'Bearer REPLACE_BEARER_TOKEN',
-     'x-ibm-client-id': 'REPLACE_THIS_KEY' } };
+// var options = { method: 'GET',
+//   url: 'https://apigw.it.umich.edu/um/Curriculum/SOC/Terms/REPLACE_TERMCODE/Schools/REPLACE_SCHOOLCODE/Subjects/REPLACE_SUBJECTCODE/CatalogNbrs/REPLACE_CATALOGNUMBER/Sections/REPLACE_SECTIONNUMBER/Meetings',
+//   headers: 
+//    { accept: 'application/json',
+//      authorization: 'Bearer REPLACE_BEARER_TOKEN',
+//      'x-ibm-client-id': 'REPLACE_THIS_KEY' } };
 
-request(options, function (error, response, body) {
-  if (error) return console.error('Failed: %s', error.message);
+// request(options, function (error, response, body) {
+//   if (error) return console.error('Failed: %s', error.message);
 
-  console.log('Success: ', body);
-});
+//   console.log('Success: ', body);
+// });
 // 
 
-app.listen(3000, function() {
+app.listen(process.event.PORT || 3000, function() {
 	console.log('Umich course app running on port 3000');
 });
